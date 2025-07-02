e806e9b3d74fb17d7ffb2c1c9fbcd3ca7ca9c468
Sude Ural <sudeural@be3e0957-f1d6-404d-a6a8-14c5a8c3c686.fritz.box>
2025-07-02 19:48:00 +0200

    Initial Maven project structure

---
+++ src/main/java/com/sude/myprose/App.java
@@ -0,0 +1,13 @@
+package com.sude.myprose;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
